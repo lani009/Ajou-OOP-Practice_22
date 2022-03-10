c83d75cd09a5e94df6b9aecfdc9e964aa8b4992d
Lani Jung <lani009@naver.com>
2022-03-10 17:08:36 +0900

    Update ABFinder.java

--- 실습문제 코드/2주차/ABFinder.java
+++ 실습문제 코드/2주차/ABFinder.java
@@ -3,7 +3,15 @@ public class ABFinder {
         int[][] abArray = new int[100][2]; // A와 B의 조합을 담는 배열
         int cnt = 0; // 조합의 개수
 
-        // 이 부분을 채워주세요.
+        for (int i = 0; i < 10; i++) {  // A
+            for (int j = 0; j < 10; j++) {  // B
+                if (((i*10+j) + (j*10+i)) == 99) {
+                    abArray[cnt][0] = i;
+                    abArray[cnt][1] = j;
+                    cnt++;
+                }
+            }
+        }
 
         for (int i = 0; i < cnt; i++) {
             System.out.printf("%d %d\n", abArray[i][0], abArray[i][1]);
