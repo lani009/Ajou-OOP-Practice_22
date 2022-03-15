9c825c3c401e6270a38a3b10cf4d6ad7a6c68916
Lani Jung <lani009@naver.com>
2022-03-16 00:18:41 +0900

    add: 3week RegularPolygon

---
+++ 실습문제 코드/3주차/RegularPolygon.java
@@ -0,0 +1,32 @@
+public class RegularPolygon {
+    private int numOfAngle;  // 각의 개수
+
+    public RegularPolygon(int numOfAngle) {
+        this.numOfAngle = numOfAngle;
+    }
+
+    public int getNumOfAngle() {
+
+    }
+
+    /**
+     * 내각의 합
+     */
+    public int getSumOfInternalAngles() {
+
+    }
+
+    /**
+     * 한 내각의 크기
+     */
+    public int getInternalAngle() {
+
+    }
+
+    /**
+     * 한 외각의 크기
+     */
+    public int getExteriorAngle() {
+
+    }
+}
